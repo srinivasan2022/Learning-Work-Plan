36d952eb9332ab02c4d098f9adb75e08d1d16aa9
srinivasan2022 <srivijay6385@gmail.com>
2026-07-21 11:06:29 +0530

    Learn

--- Week-01-Fundamentals/Day-01/Program.cs
+++ Week-01-Fundamentals/Day-01/Program.cs
@@ -11,4 +11,14 @@ namespace HelloWorld
   }
 }
 
+// using System means that we can use classes from the System namespace
+
+// namespace is used to organize your code, and it is a container for classes and other namespaces
+
+// class is a container for data and methods, 
+// Every line of code that runs in C# must be inside a class. In our example, we named the class Program
+
+// Console is a class of the System namespace
+// System.Console.WriteLine() to print/output text , we can write --> Console.WriteLine()
+
 
---
+++ Week-01-Fundamentals/Day-02/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //variables();
+      //constvar();
+      //multvar();
+      //TypeCast();
+      //UserInput();
+      Operators();
+    }
+
+    static void variables()  // Syntax : type variableName = value;
+    {
+      Console.WriteLine("Hello variables");  
+      int n1 = 5;            // int - stores integers (whole numbers), without decimals, such as 123 or -123
+      Console.WriteLine(n1);
+
+
+      double d1 = 19.55;    // double - stores floating point numbers, with decimals, such as 19.99 or -19.99
+      Console.WriteLine(d1);
+
+      char c1 = 'D';      // char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+      Console.WriteLine(c1);
+
+      bool b1 = true;    // bool - stores values with two states: true or false
+      Console.WriteLine(b1);
+
+      string text = "Hello";  // string - stores text, such as "Hello World". String values are surrounded by double quotes
+      Console.WriteLine(text);
+
+    }
+    
+    static void constvar(){
+        const int cn1 = 10;
+        //cn1 = 20;  // --> can't changed , its through error
+        Console.WriteLine(cn1);
+    }
+
+    static void multvar(){
+        int x = 5, y = 6, z = 50;
+        Console.WriteLine(x + y + z); // --> 61 --> return as int
+
+        int a, b, c;
+        a = b = c = 50;
+        Console.WriteLine("abc "+ a + b + c); // --> abc 505050 --> return as string
+
+        string name1 = "Srini";
+       string name2 = "QS";
+       Console.WriteLine("FullName " + name1 + name2); // --> FullName SriniQS
+    }
+
+    static void TypeCast(){
+
+      int int1 = 8;
+      double doub1 = int1 ;  //Automatic casting --> 8
+
+      Console.WriteLine(int1);
+      Console.WriteLine("Automatoc casting : int To double :" + doub1);
+
+      double doub2 = 15.89;
+      int int2 = (int) doub2;  // Manual casting --> 15
+      Console.WriteLine(doub2);
+      Console.WriteLine(int2);
+
+//       C# Type Casting
+// Type casting is when you assign a value of one data type to another type.
+
+// In C#, there are two types of casting:
+
+// Implicit Casting (automatically) - converting a smaller type to a larger type size
+// char -> int -> long -> float -> double
+
+// Explicit Casting (manually) - converting a larger type to a smaller size type
+// double -> float -> long -> int -> char
+    
+  }
+
+  static void UserInput(){
+
+    Console.WriteLine("Enter the name :");
+    String name = Console.ReadLine();
+    Console.WriteLine("My name is " + name); // My name is Srini
+
+    Console.WriteLine("Enter the age :");
+    //int age = Console.ReadLine();   // Its through Error 
+     int age = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("My age is " + age); 
+
+    //Console.ReadLine() --> Always return as string
+  }
+  static void Operators(){
+
+    int num1 = 30;
+    int num2 = 10;
+    //int sub2 = num1- num2; // 20
+
+    Console.WriteLine("Add "+ Convert.ToInt32(num1+num2)); // 40
+    //Console.WriteLine("Add "+ num1+num2); // Its return --> Add 3010
+    //Console.WriteLine("Sub "+ num1-num2); // Its return Error
+    Console.WriteLine("Sub "+ Convert.ToInt32(num1-num2)); // 20
+    Console.WriteLine("Mul "+ Convert.ToInt32(num1*num2)); // 300
+    Console.WriteLine("Div "+ Convert.ToInt32(num1/num2)); // 3
+    Console.WriteLine("Mod "+ Convert.ToInt32(num1%num2)); // 0
+
+  }
+
+  }
+
+}
+
